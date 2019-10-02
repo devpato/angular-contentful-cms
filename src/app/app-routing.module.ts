4eae0758c51389ab72518b4a4db6f0a3cdf28032
Patricio Vargas <pevargas2730@gmail.com>
2019-10-02 01:56:05 -0400

    aroute created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostsPageComponent } from './posts-page/posts-page.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'posts',
     component: PostsPageComponent
+  },
+  {
+    path: 'posts/:id',
+    component: PostDetailsComponent
   }
 ];
 
