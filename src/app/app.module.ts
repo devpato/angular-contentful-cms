f9f177eb09c596841b22f0d999c46548d1a75b34
Patricio Vargas <pevargas2730@gmail.com>
2019-10-02 01:56:14 -0400

    added component to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsPageComponent } from './posts-page/posts-page.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostsPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PostsPageComponent, PostDetailsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
