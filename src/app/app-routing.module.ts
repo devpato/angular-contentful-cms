d9a1aaa0790dfa97bd137341e2de4b5e3e5af9f5
Patricio Vargas <pevargas2730@gmail.com>
2019-10-02 01:31:13 -0400

    post page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsPageComponent } from './posts-page/posts-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PostsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
