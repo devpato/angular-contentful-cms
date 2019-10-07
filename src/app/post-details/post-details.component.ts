026d5e748ad460ee571b3dec2b58f2596563b9d1
Patricio Vargas <pevargas2730@gmail.com>
2019-10-07 18:05:36 -0400

    getting post based on id

--- src/app/post-details/post-details.component.ts
+++ src/app/post-details/post-details.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ContentfulService } from '../contentful.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
   styleUrls: ['./post-details.component.scss']
 })
-export class PostDetailsComponent implements OnInit {
+export class PostDetailsComponent {
+  id = this.route.snapshot.paramMap.get('id');
+  post$ = this.contentfulService.getPost(this.id);
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private contentfulService: ContentfulService, private route: ActivatedRoute) {}
 }
